25d92e05b8782bd080bf9841c0d1482f6e1d90dc
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-08-23 21:41:43 +0200

    attempting getting somehow correct timestamp

--- syslog/handler.go
+++ syslog/handler.go
@@ -5,12 +5,19 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/mcuadros/go-syslog.v2"
+)
+
+var (
+	mutex             sync.RWMutex
+	printersTimestamp = map[string]int64{}
+	pritnersMsg       = map[string]int64{}
 )
 
 func startSyslogServer(listenUDP string) (syslog.LogPartsChannel, *syslog.Server) {
@@ -38,7 +45,7 @@ func MetricsListener(listenUDP string, influxURL string, influxToken string, inf
 		for logParts := range channel {
 			log.Trace().Msg(fmt.Sprintf("%v", logParts))
 
-			sentToInflux(processTimestamps(logParts["message"].(string)), writeAPI)
+			sentToInflux(processTimestamps(logParts["message"].(string), logParts["hostname"].(string)), writeAPI)
 		}
 	}(channel)
 
@@ -46,8 +53,34 @@ func MetricsListener(listenUDP string, influxURL string, influxToken string, inf
 
 }
 
-func processTimestamps(message string) (result []string) {
+func processTimestamps(message string, mac string) (result []string) {
+
 	messageSplit := strings.Split(message, "\n")
+
+	firstMsg := strings.Split(messageSplit[0], " ")
+	messageSplit[0] = firstMsg[1]
+
+
+	printerInfo := strings.Split(firstMsg[0], ",")
+	msg := strings.Split(printerInfo[0], "=")[1]
+	printerTm := strings.Split(printerInfo[1], "=")[1]
+
+
+	var printerStart int64
+
+	mutex.Lock()
+	_, contains := printersTimestamp[mac]
+	if !contains {
+		if printerTm < 10000000 { // allow 10 ms drift
+			printersTimestamp[mac] = time.Now().UnixNano() - printerTm
+		} else {
+			printersTimestamp[mac] = -1
+
+		}
+	} else {
+		printerStart = printersTimestamp[mac]
+	}
+	mutex.Unlock()
 
 	for i, line := range strings.Split(message, "\n") {
 		splitted := strings.Split(line, " ")
@@ -56,7 +89,7 @@ func processTimestamps(message string) (result []string) {
 			log.Info().Msg("Expected error while parsing timestamp: " + err.Error())
 			continue
 		}
-		splitted[len(splitted)-1] = strconv.FormatInt(time.Now().UnixNano()+delta, 10)
+		splitted[len(splitted)-1] = strconv.FormatInt(calculateTimestamp(printerStart, printerTm, delta), 10)
 		log.Trace().Msg("Processing timestamps for " + message)
 		messageSplit[i] = strings.Join(splitted, " ")
 	}
@@ -90,4 +123,25 @@ func fixLine(line string) (message []string) {
 	}
 	return []string{}
 
+}
+
+func calculateTimestamp(printerStart int64, printerRunning int64, metricDelta int64) (result int64) {
+	if printerStart == -1 {
+		return time.Now().UnixNano() + metricDelta
+	}
+	return printerStart + printerRunning + metricDelta
+}
+
+func checkPrinterRestart(messageSplitted []string) (printerTm int64, result bool, err error) {
+	msg := strings.Split(messageSplitted[0], ",")
+	msgItem := strings.Split(msg[0], "=")[0]
+	printerInterval := strings.Split(msg[1], "=")[1]
+
+	if msgItem < 
+
+	i, err := strconv.ParseInt(strings.Split(strings.Split(messageSplitted[0], ",")[1], "=")[1], 10, 64)
+	if err != nil {
+		return 0, false, err
+	}
+	return i, true, nil
 }
