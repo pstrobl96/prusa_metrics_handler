0e15aef934b86576e9fb78bd3f04ac7c1d51d12e
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-11-17 16:51:16 +0100

    TestMetricsListener

--- handler/ingest_test.go
+++ handler/ingest_test.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"net"
 	"testing"
+	"time"
 )
 
 func TestStartSyslogServer(t *testing.T) {
@@ -19,4 +21,30 @@ func TestStartSyslogServer(t *testing.T) {
 
 	// Clean up
 	server.Kill()
+}
+
+func TestMetricsListener(t *testing.T) {
+	listenUDP := "localhost:514"
+	influxURL := "http://localhost:8086"
+	influxToken := "my-token"
+	influxBucket := "my-bucket"
+	influxOrg := "my-org"
+
+	go MetricsListener(listenUDP, influxURL, influxToken, influxBucket, influxOrg)
+
+	time.Sleep(1 * time.Second)
+
+	conn, err := net.Dial("udp", listenUDP)
+	if err != nil {
+		t.Fatalf("Failed to connect to syslog server: %v", err)
+	}
+	defer conn.Close()
+
+	message := "<34>1 2023-10-10T14:48:00Z mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8"
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		t.Fatalf("Failed to send syslog message: %v", err)
+	}
+
+	// Since the process function is not defined here and it is not supposed to return anything, I cannot verify its behavior here.
 }
