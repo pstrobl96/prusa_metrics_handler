f4725e3814e74aefd81c66f4e58316fdce189477
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2024-11-17 15:43:13 +0100

    Fixing around time processing

--- syslog/process.go
+++ syslog/process.go
@@ -2,6 +2,8 @@ package syslog
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"sync"
 	"time"
 
@@ -34,6 +36,8 @@ func process(data format.LogParts, received time.Time) {
 		return
 	}
 	log.Debug().Msg(fmt.Sprintf("Processing timestamp %d", timestamp))
+	//started := time.Unix(timestamp/1000000000, 0)
+	//log.Info().Msg(fmt.Sprintf("STARTED %s", started))
 	log.Debug().Msg(fmt.Sprintf("Processing data for printer %s", mac))
 }
 
@@ -41,29 +45,38 @@ func process(data format.LogParts, received time.Time) {
 // it is basically used for the synchronization of time between handler and the printer
 func processTimestamp(data format.LogParts, received time.Time) (string, int64, error) {
 	mac, ok := data["hostname"].(string)
-	tmValue, ok := data["tm"].(int64)
 	if !ok {
-		return "", 0, fmt.Errorf("tm value is not an int64")
+		return "", 0, fmt.Errorf("mac is not an string")
 	}
-	if !ok {
-		return "", 0, fmt.Errorf("hostname is not a string")
+	re := regexp.MustCompile(`tm=(\d+)`)
+	matches := re.FindAllStringSubmatch(data["message"].(string), -1)
+
+	for _, match := range matches {
+		if len(match) < 1 {
+			return "", 0, fmt.Errorf("none time delta value found")
+		}
 	}
+	tmValue, err := strconv.ParseInt(matches[0][1], 10, 64)
+	if err != nil {
+		return "", 0, fmt.Errorf("time delta cannot be converted to int64")
+	}
+
 	timedelta := tmValue
 	timestamp := time.Now().UnixNano()
 	printerInterface, _ := printerStates.LoadOrStore(mac, &PrinterStatus{})
 	state := printerInterface.(*PrinterStatus)
+	state.mu.Lock()
 	defer state.mu.Unlock()
 
 	// Handle the case when the printer is restarted.
 	// This is not ideal as in rare cases the value can be the same,
 	// and then it will not work correctly. At the moment, it's better than nothing.
 
-	if state.LastDelta < timedelta || state.FirstTimestamp == 0 {
+	if state.FirstTimestamp == 0 {
 		state.FirstTimestamp = received.Add(-time.Duration(timedelta)).UnixNano()
 		timestamp = received.Add(-time.Duration(timedelta)).UnixNano()
 		state.LastDelta = timedelta
 	} else {
-		timestamp = state.FirstTimestamp
 		state.LastDelta = timedelta
 	}
 	return mac, timestamp, nil
